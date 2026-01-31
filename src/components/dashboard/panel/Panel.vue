17ef8a95bf0fabd782cdae49adad73bdaaa29880
Filimon4 <f.rykov@bk.ru>
2026-01-31 22:15:46 +0300

    add styles for panel

--- src/components/dashboard/panel/Panel.vue
+++ src/components/dashboard/panel/Panel.vue
@@ -1,32 +1,33 @@
 <template>
   <div
     class="panel-container"
-    :style="{width: panelOpened ? '100px' : '50px'}"
+    :style="{width: panelOpened ? '200px' : '50px'}"
   >
     <div 
       class="panel-item"
+      :class="panelOpened ? 'panel-arrow-close' : ''"
       @click="panelOpened = !panelOpened"
-      :style="{
-        transform: panelOpened ? 'rotate(180deg)' : null,
-        justifyContent: panelOpened ? 'left' : 'center'
-      }"
     >
-      <img class="panel-arrow"  src="/public/icons/leftToRightArrow.png" width="30" height="30" />
+      <img src="/public/icons/leftToRightArrow.png" width="30" height="30" />
     </div>
-    <div v-if="!panelOpened" class="panel-content">
-      <div class="panel-content-block">
-        <div v-for="(item, index) in panelItems.first" :key="index" class="panel-item">
+    <div
+      class="panel-content"
+      :class="{ 'panel-content-opened': panelOpened }"
+    >
+      <div
+        v-for="block in panelBlocks"
+        :key="block.key"
+        class="panel-content-block"
+      >
+        <div
+          v-for="item in block.items"
+          :key="item.id"
+          class="panel-item"
+        >
           <img :src="item.icon" width="30" height="30" alt="" />
+          <span v-if="panelOpened">{{ item.name }}</span>
         </div>
       </div>
-      <div class="panel-content-block">
-        <div v-for="(item, index) in panelItems.second" :key="index" class="panel-item">
-          <img :src="item.icon" width="30" height="30" alt="" />
-        </div>
-      </div>
-    </div>
-    <div v-if="panelOpened" class="panel-content-opened">
-
     </div>
   </div>
 </template>
@@ -35,32 +36,25 @@
 import marketingIcon from '/icons/marketing.png'
 import projectIcon from '/icons/project.png'
 import accountIcon from '/icons/account.png'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const activeItem = ref(1)
 const panelOpened = ref(false)
 
 const panelItems = ref({
   first: [
-    {
-      id: 1,
-      name: 'Маркетинг',
-      icon: marketingIcon
-    },
-    {
-      id: 2,
-      name: 'Проект',
-      icon: projectIcon
-    },
+    { id: 1, name: 'Маркетинг', icon: marketingIcon },
+    { id: 2, name: 'Проект', icon: projectIcon },
   ],
   second: [
-    {
-      id: 3,
-      name: "Аккаунт",
-      icon: accountIcon
-    }
-  ]
+    { id: 3, name: 'Аккаунт', icon: accountIcon },
+  ],
 })
+
+const panelBlocks = computed(() => [
+  { key: 'first', items: panelItems.value.first },
+  { key: 'second', items: panelItems.value.second },
+])
 
 </script>
 
@@ -90,6 +84,7 @@ const panelItems = ref({
   flex-direction: column;
   align-items: center;
   gap: 10px;
+  width: 100%;
 }
 
 .panel-item {
@@ -97,10 +92,25 @@ const panelItems = ref({
   justify-content: center;
   align-items: center;
   
+  width: 100%;
   height: 40px;
 }
 
 .panel-content-opened {
-  width: 100px;
+  align-items: start;
+  padding-left: 10px;
+
+  .panel-item {
+    justify-content: start;
+    gap: 10px;
+    color: white;
+    font-size: 20px;
+  }
+}
+
+.panel-arrow-close {
+  transform: rotate(180deg);
+  justify-content: left;
+  padding-left: 10px;
 }
 </style>
