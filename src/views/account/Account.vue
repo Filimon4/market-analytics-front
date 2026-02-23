f0a46723eaab37327eaf60e78c622b120e25aa56
Filimon4 <f.rykov@bk.ru>
2026-02-23 15:27:47 +0300

    feature: add project in account

--- src/views/account/Account.vue
+++ src/views/account/Account.vue
@@ -6,29 +6,55 @@
             </div>
             <div v-if="error" class="error-message">{{ error }}</div>
             <n-spin :show="loading">
-            <n-grid :cols="1" class="info-content" :y-gap='10'>
-                <n-grid-item class="content-block">
-                    <div class="block-label">Имя</div>
-                    <div class="block-content">{{ user?.name ?? '—' }}</div>
-                </n-grid-item>
-                <n-grid-item class="content-block">
-                    <div class="block-label">Почта</div>
-                    <div class="block-content">{{ user?.email ?? '—' }}</div>
-                </n-grid-item>
-                <n-grid-item class="content-block">
-                    <div class="block-label">Роль</div>
-                    <div class="block-content">{{ user?.role ?? '—' }}</div>
-                </n-grid-item>
-                <n-grid-item class="content-block">
-                    <div class="block-label">Дата регистрации</div>
-                    <div class="block-content">{{ formattedCreatedAt }}</div>
-                </n-grid-item>
-                <n-grid-item class="content-block">
-                    <div class="block-label">Статус</div>
-                    <div class="block-content">{{ user?.status ?? '—' }}</div>
-                </n-grid-item>
-            </n-grid>
+                <n-grid :cols="1" class="info-content" :y-gap='10'>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Имя</div>
+                        <div class="block-content">{{ user?.name ?? '—' }}</div>
+                    </n-grid-item>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Почта</div>
+                        <div class="block-content">{{ user?.email ?? '—' }}</div>
+                    </n-grid-item>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Дата регистрации</div>
+                        <div class="block-content">{{ formattedCreatedAt }}</div>
+                    </n-grid-item>
+                </n-grid>
             </n-spin>
+        </div>
+        <div class="info-container">
+            <div class="info-label">
+                Проект
+            </div>
+            <div v-if="error" class="error-message">{{ error }}</div>
+            <n-spin :show="loading">
+                <n-grid :cols="1" class="info-content" :y-gap='10'>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Текущий проект</div>
+                        <div class="block-content">{{ '—' }}</div>
+                    </n-grid-item>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Роль в проекте</div>
+                        <div class="block-content">{{ user?.role ?? '—' }}</div>
+                    </n-grid-item>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Дата входа</div>
+                        <div class="block-content">{{ formattedCreatedAt }}</div>
+                    </n-grid-item>
+                    <n-grid-item class="content-block">
+                        <div class="block-label">Статус</div>
+                        <div class="block-content">{{ user?.status ?? '—' }}</div>
+                    </n-grid-item>
+                </n-grid>
+            </n-spin>
+            <div class="container-actions">
+                <div class="action">
+                    Поменять проект
+                </div>
+                <div class="action">
+                    Добавить проект
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -75,6 +101,9 @@ onMounted(fetchCurrentUser)
     height: 100%;
     width: 100%;
     padding: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 }
 
 .info-container {
@@ -95,13 +124,27 @@ onMounted(fetchCurrentUser)
     height: fit-content;
     width: 100%;
     padding: 7px;
-    margin-top: 5px;
+    margin-block: 5px;
 }
 
 .content-block {
     display: flex;
     gap: 10px;
     align-items: center;
+}
+
+.container-actions {
+    width: 100%;
+    display: flex;
+    justify-content: right;
+    gap: 10px;
+}
+
+.action {
+    padding: 5px 10px;
+    background-color: rgba(128, 128, 128, 0.212);
+    border-radius: 7px;
+    cursor: pointer;
 }
 
 .block-label {
