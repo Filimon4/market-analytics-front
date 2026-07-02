7ce43d3e88c65cd6c3982c99729f3db060e69720
Filimon4 <f.rykov@bk.ru>
2026-07-02 22:35:33 +0300

    build new config for docker

---
+++ vite.build.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          '@use "@/src/assets/styles/variables.scss" as *; @use "@/src/assets/styles/mixins.scss" as *;',
+      },
+    },
+  },
+})
